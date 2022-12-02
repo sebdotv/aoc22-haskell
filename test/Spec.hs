4e9c351257774cbeb020e8460d58760fe0786fa8
sebdotv <sebdotv@gmail.com>
2022-12-02 14:49:36 +0100

    add 02

--- test/Spec.hs
+++ test/Spec.hs
@@ -6,17 +6,17 @@ main :: IO ()
 main = hspec $ do
   describe "day01 P1" $ do
     it "works for example" $ do
-      ex0in <- liftIO (Day01.solveP1 <$> readLines "./data/011example.txt")
+      ex0in <- liftIO (Day01.solveP1 <$> readLines "./data/01example.txt")
       ex0in `shouldBe` 24000
     it "works for input" $ do
-      ex0in <- liftIO (Day01.solveP1 <$> readLines "./data/011input.txt")
+      ex0in <- liftIO (Day01.solveP1 <$> readLines "./data/01input.txt")
       ex0in `shouldBe` 70613
   describe "day01 P2" $ do
     it "works for example" $ do
-      ex0in <- liftIO (Day01.solveP2 <$> readLines "./data/011example.txt")
+      ex0in <- liftIO (Day01.solveP2 <$> readLines "./data/01example.txt")
       ex0in `shouldBe` 45000
     it "works for input" $ do
-      ex0in <- liftIO (Day01.solveP2 <$> readLines "./data/011input.txt")
+      ex0in <- liftIO (Day01.solveP2 <$> readLines "./data/01input.txt")
       ex0in `shouldBe` 205805
   where
     readLines name = lines <$> readFile name
