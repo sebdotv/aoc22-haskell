a62b9a0dcd7416332934f2a3e64f469c11a2ab76
sebdotv <sebdotv@gmail.com>
2022-12-02 17:08:26 +0100

    simplify tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -3,23 +3,24 @@ import Control.Monad.IO.Class (liftIO)
 import Day01
 import Day02
 import Test.Hspec
+import Text.Printf
 
 main :: IO ()
 main = hspec $ do
   describe "day01 P1" $ do
-    it "works for example" $ test Day01.solveP1 "./data/01example.txt" 24000
-    it "works for input" $ test Day01.solveP1 "./data/01input.txt" 70613
+    testPart Day01.solveP1 "01" 24000 70613
   describe "day01 P2" $ do
-    it "works for example" $ test Day01.solveP2 "./data/01example.txt" 45000
-    it "works for input" $ test Day01.solveP2 "./data/01input.txt" 205805
+    testPart Day01.solveP2 "01" 45000 205805
   describe "day02 P1" $ do
-    it "works for example" $ test Day02.solveP1 "./data/02example.txt" 15
-    it "works for input" $ test Day02.solveP1 "./data/02input.txt" 11386
+    testPart Day02.solveP1 "02" 15 11386
   describe "day02 P2" $ do
-    it "works for example" $ test Day02.solveP2 "./data/02example.txt" 12
-    it "works for input" $ test Day02.solveP2 "./data/02input.txt" 13600
+    testPart Day02.solveP2 "02" 12 13600
   where
     readLines name = lines <$> readFile name
+
+    testPart solver day solutionExample solutionInput = do
+      it "works for example" $ test solver (printf "./data/%s-example.txt" day) solutionExample
+      it "works for input" $ test solver (printf "./data/%s-input.txt" day) solutionInput
 
     test :: Common.Solver -> String -> Int -> IO ()
     test solver file solution = do
